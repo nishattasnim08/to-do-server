a9df51f2d3e6b35142ccb247ea78be948ad2a00b
nishattasnim08 <nishattasnim24258@gmail.com>
2022-07-02 00:58:46 +0600

    todo list fetched

--- index.js
+++ index.js
@@ -9,10 +9,51 @@ app.use(express.json())
 
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("ajke amar mon valo nei");
-});
+// app.get("/", (req, res) => {
+//   res.send("ajke amar mon valo nei");
+// });
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@todo.kbflljo.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
+async function run() {
+    try {
+        await client.connect();
+        const toDoCollection = client.db("tododatabase").collection("todolist");
+
+        app.get("/todo", async (req, res) => {
+            const query = {completed: false};
+            const cursor = toDoCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        app.get("/completed", async (req, res) => {
+            const query = {completed: true};
+            const cursor = toDoCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+        
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
 
 app.listen(port, () => {
-  console.log("runing to do");
+    console.log("runing to do");
 });
